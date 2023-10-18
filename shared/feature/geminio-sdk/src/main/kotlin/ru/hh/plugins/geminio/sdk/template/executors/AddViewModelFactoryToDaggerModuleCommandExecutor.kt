bc716a83b300097d126a564b3bd8c7215ebeb7e9
daniiltimonin <rand1kch@gmail.com>
2023-10-18 20:34:20 +0200

    add is dry check

--- shared/feature/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/AddViewModelFactoryToDaggerModuleCommandExecutor.kt
+++ shared/feature/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/AddViewModelFactoryToDaggerModuleCommandExecutor.kt
@@ -21,67 +21,71 @@ internal fun RecipeExecutor.execute(
     command: RecipeCommand.AddViewModelFactoryToDaggerModule,
     executorData: GeminioRecipeExecutorData
 ) = with(executorData) {
-    HHLogger.d("AddViewModelFactoryToDaggerModule command [$command]")
+    if (executorData.isDryRun) {
+        HHLogger.d("AddViewModelFactoryToDaggerModule command [$command] ${executorData.isDryRun}")
 
-    val featureName = command.featureName.evaluateString(
-        targetDirectory,
-        moduleTemplateData,
-        existingParametersMap
-    ) ?: return@with
+        val featureName = command.featureName.evaluateString(
+            targetDirectory,
+            moduleTemplateData,
+            existingParametersMap
+        ) ?: return@with
 
-    val componentPath = command.componentPath.evaluateString(
-        targetDirectory,
-        moduleTemplateData,
-        existingParametersMap
-    ) ?: return@with
+        val componentPath = command.componentPath.evaluateString(
+            targetDirectory,
+            moduleTemplateData,
+            existingParametersMap
+        ) ?: return@with
 
-    val componentName = command.componentName.evaluateString(
-        targetDirectory,
-        moduleTemplateData,
-        existingParametersMap
-    ) ?: return@with
+        val componentName = command.componentName.evaluateString(
+            targetDirectory,
+            moduleTemplateData,
+            existingParametersMap
+        ) ?: return@with
 
-    val featurePackage = command.featurePackage.evaluateString(
-        targetDirectory,
-        moduleTemplateData,
-        existingParametersMap
-    ) ?: return@with
+        val featurePackage = command.featurePackage.evaluateString(
+            targetDirectory,
+            moduleTemplateData,
+            existingParametersMap
+        ) ?: return@with
 
-    val featureComponentPath = File(componentPath).toPsiDirectory(executorData.project) ?: return
-    val featureComponentPsi = featureComponentPath.findFile(componentName) as KtFile
+        val featureComponentPath =
+            File(componentPath).toPsiDirectory(executorData.project) ?: return
+        val featureComponentPsi = featureComponentPath.findFile(componentName) as KtFile
 
-    val featureComponentImportList = featureComponentPsi.importList ?: return@with
-    val importListFactory = KtPsiFactory(featureComponentImportList.project)
-    featureComponentImportList.add(importListFactory.createNewLine())
-    featureComponentImportList.add(
-        importListFactory.createExpression("import $featurePackage.di.${featureName}ViewModelFactory")
-    )
-
-    val featureComponentClassPsi =
-        featureComponentPsi.children.find { it is KtClass } as? KtClass? ?: return@with
-    val featureComponentClassBodyPsi =
-        featureComponentClassPsi.children.find { it is KtClassBody } as? KtClassBody ?: return@with
-    val featureComponentClassBodyFactory = KtPsiFactory(featureComponentClassBodyPsi.project)
-    val lastFunction = featureComponentClassBodyPsi.children.findLast { it is KtNamedFunction }
-
-    lastFunction?.let {
-        featureComponentClassPsi.addAfter(
-            featureComponentClassBodyFactory.createViewModelFactoryMethod(
-                featureName
-            ),
-            lastFunction
+        val featureComponentImportList = featureComponentPsi.importList ?: return@with
+        val importListFactory = KtPsiFactory(featureComponentImportList.project)
+        featureComponentImportList.add(importListFactory.createNewLine())
+        featureComponentImportList.add(
+            importListFactory.createExpression("import $featurePackage.di.${featureName}ViewModelFactory")
         )
-        featureComponentClassPsi.addAfter(
-            featureComponentClassBodyFactory.createNewLine(),
-            lastFunction
-        )
-        featureComponentClassPsi.addAfter(
-            featureComponentClassBodyFactory.createNewLine(),
-            lastFunction
-        )
+
+        val featureComponentClassPsi =
+            featureComponentPsi.children.find { it is KtClass } as? KtClass? ?: return@with
+        val featureComponentClassBodyPsi =
+            featureComponentClassPsi.children.find { it is KtClassBody } as? KtClassBody
+                ?: return@with
+        val featureComponentClassBodyFactory = KtPsiFactory(featureComponentClassBodyPsi.project)
+        val lastFunction = featureComponentClassBodyPsi.children.findLast { it is KtNamedFunction }
+
+        lastFunction?.let {
+            featureComponentClassPsi.addAfter(
+                featureComponentClassBodyFactory.createViewModelFactoryMethod(
+                    featureName
+                ),
+                lastFunction
+            )
+            featureComponentClassPsi.addAfter(
+                featureComponentClassBodyFactory.createNewLine(),
+                lastFunction
+            )
+            featureComponentClassPsi.addAfter(
+                featureComponentClassBodyFactory.createNewLine(),
+                lastFunction
+            )
+        }
+
+        CodeStyleManager.getInstance(project).reformat(featureComponentPsi)
     }
-
-    CodeStyleManager.getInstance(project).reformat(featureComponentPsi)
 }
 
 private fun KtPsiFactory.createViewModelFactoryMethod(featureName: String) = createFunction(
